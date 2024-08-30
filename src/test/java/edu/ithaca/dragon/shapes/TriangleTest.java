76bc589f77078a5c70685b2ed786b177e652ab9b
CodingClassOmar <OHJawaid@gmail.com>
2024-08-30 16:22:33 -0400

    Created multiple tests for different failed constructors

--- src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -9,6 +9,14 @@ public class TriangleTest {
 
     @Test
     public void invalidConstructorTests(){
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(0,0,0));
+
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(1,0,0));
+
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(0,1,1));
+
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(-1,0,0));
+        
 
     }
     
