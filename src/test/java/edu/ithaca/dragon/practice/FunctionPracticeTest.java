006a839ab9adb7e4adbe6a84b90244d8f852aab5
ohjawaid <ohjawaid@gmail.com>
2024-09-04 12:38:44 -0400

    Removing unused import

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -2,7 +2,7 @@ package edu.ithaca.dragon.practice;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
+
 
 import org.junit.jupiter.api.Test;
 import java.util.List;
